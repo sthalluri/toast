b9c69f8e0b483abeb8c37b312a832fdb19d334e3
Suresh Thalluri <sthalluri@gmail.com>
2011-05-31 18:31:48 -0400

    New Files

---
+++ js/questionPanel.js
@@ -0,0 +1,101 @@
+QuestionPanel = Ext.extend(Ext.form.FormPanel, 
+{	
+    scroll: 'vertical',
+    standardSubmit : false,
+    title: 'Add Question',
+
+	initComponent : function() {
+
+		this.formFields = [ {
+				xtype : 'textareafield',
+				name : 'question',
+				useClearIcon : true,
+				height : 100,
+				maxRows : 10,
+				autoCapitalize : false
+			}, 
+			{
+				layout:'hbox',
+				flex:1,
+           	 	defaults: {xtype: 'button', flex:1, style: 'margin: .5em;'},
+				items:[
+						new Ext.Button({
+			                text: 'Save',
+							scope: this,
+			                width:100,
+			                ui : 'confirm',
+			                handler: this.save
+			            }), 
+						new Ext.Button({
+			                text: 'Reset',
+							scope: this,
+				        	ui:'decline',
+			                width:100,
+			                handler: function(){
+			                	this.reset();
+			                }
+			            })
+				]
+			
+			}
+		];
+
+        this.items= [{
+                xtype: 'fieldset',
+    			title : 'Notes',
+                defaults: {
+                    required: true,
+                    labelAlign: 'left',
+                    labelWidth: '40%'
+                },
+                items: this.formFields
+            }
+        ];
+    
+        this.dockedItems = [
+            {
+                xtype: 'toolbar',
+                dock: 'top',
+                title:'Edit Meeting',
+                items: [
+                    {
+                        text: 'Back',
+                        ui: 'back',
+                        scope:this,
+                        handler: function() {
+                        	this.hide();
+                        	tableTopicPanel.listMode();
+                        	tableTopicPanel.show();
+                        }
+                    }
+                ]
+            }
+        ];
+    
+        QuestionPanel.superclass.initComponent.call(this);	
+	},
+	updateMessage: function(msg){
+		this.items.get(0).titleEl.setHTML('<div class="msg"><p >'+msg+'</p></div>');
+	},
+	loadQuestion: function(pQuestion){
+		this.reset();
+		for(var i=0; i< this.fields.items.length ; i++){
+			var comp = this.fields.items[i];
+			if(comp.name == 'question'){
+				comp.setValue(pQuestion.text);
+			}
+		}
+		this.question = pQuestion;
+	},
+	save : function(){
+		var values = this.getValues();
+		this.question.text = values.question;
+		for(var i=0 ; i<questionDataStore.data.length; i++){
+			var qData = questionDataStore.getAt(i).data;
+			if(qData.id == this.question.id){
+				qData.text = values.question;
+			}
+		}
+		this.controller.saveTableTopics();
+	}
+});
